10d83d92ce808be37a4085136f3e9d651e3a18a1
TinyTools Dev <dev@tinytools.local>
2026-07-14 17:32:02 -0400

    Updated

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -1,6 +1,7 @@
 // This program totals and averages the sales amounts for any
 // number of days. The amounts are stored in a dynamically 
 // allocated array. 
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
