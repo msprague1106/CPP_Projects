ff77e15e94e4ec320e993393617fe37afe30b699
TinyTools Dev <dev@tinytools.local>
2026-09-21 19:36:49 -0400

    update

--- testPointers.cpp
+++ testPointers.cpp
@@ -1,9 +1,13 @@
 //Test pointers
-#include<iostream>;
+#include<iostream>
 using namespace std;
 
 int main()
 {
+
+    // pointers = variables that stores a memory address 
+    // of anther variable 
+    // sometime it's easier to work with an address
 
     // & address of operator
     // * deference operator
