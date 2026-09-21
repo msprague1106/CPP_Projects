bb38f42401642a1ead22cd494a68d59350e45b4d
TinyTools Dev <dev@tinytools.local>
2026-09-21 19:29:47 -0400

    updated

---
+++ testPointers.cpp
@@ -0,0 +1,28 @@
+//Test pointers
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+
+    // & address of operator
+    // * deference operator
+
+    string name = "Matthew John Sprague";
+    int age = 25;
+    string freePizzas[5] = {"pizza", "cheese pizza", "noo pizza", "pepperoni pizza", "my pizza" };
+
+    string *pName = &name;
+    int *pAge = &age;
+    string *pfreePizzas = freePizzas;
+
+    cout << *pName;
+    cout << " is " << *pAge << endl;
+    cout << endl;
+    cout << *freePizzas << endl; 
+    cout << endl;
+    cout << freePizzas[2] << '\n' << endl;
+
+
+    return 0;
+}
